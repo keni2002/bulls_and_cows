2db540426feb5603e72fcfe28bfb1a7cec7781f4
keni2002 <yerbisuniverses@gmail.com>
2025-01-01 11:45:13 -0500

    tracking files

---
+++ backend/notes/views.py
@@ -0,0 +1,29 @@
+from rest_framework import generics
+from .serializers import NoteSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Note
+
+# Create your views here.
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+            #custom save, cuz user is just read_only
+        else:
+            print(serializer.errors)
+
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
